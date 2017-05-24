8f1d2faf73484224ac69c57f017832adf27022fb
scotwilliamson <scottwilliamson@crescentschool.org>
2017-05-23 21:28:40 -0400

    Update BackgroundLogic.java

--- BackgroundLogic.java
+++ BackgroundLogic.java
@@ -19,7 +19,7 @@ public class BackgroundLogic extends Main {
 	private static Rectangle j = new Rectangle();
 	private static ArrayList<Rectangle> upperPoles = new ArrayList<Rectangle>();
 	private static ArrayList<Rectangle> lowerPoles = new ArrayList<Rectangle>();
-	private static Rectangle hitbox = new Rectangle(Xpos - 15, Ypos - 15, 30, 30);
+	private static Rectangle hitbox = new Rectangle(Xpos-15, Ypos-15, 30, 30);
 
 	public static void create() {
 		upperPoles.add(a);
@@ -49,17 +49,18 @@ public class BackgroundLogic extends Main {
 			lowerPoles.get(i).setHeight(600);
 			lowerPoles.get(i).setY((int) rectHeight[i] + 150);
 		}
-		hitbox.setX(Xpos - 15);
-		hitbox.setY(Ypos - 15);
+		hitbox.setX(Xpos-15);
+		hitbox.setY(Ypos-15);
 	}
 
-	public static boolean collisionDetection() {
+	public static int collisionDetection() {
 		for (int i = 0; i < upperPoles.size(); i++) {
 			if (hitbox.getX() + hitbox.getWidth() > lowerPoles.get(i).getX()
 					&& hitbox.getX() < lowerPoles.get(i).getX() + 50) {
 				if (hitbox.getY() < upperPoles.get(i).getHeight() || hitbox.getY() + 30 > lowerPoles.get(i).getY()) {
-					isAlive = false;
+					isAlive = 1;
 				}
+				
 			}
 		}
 		return isAlive;
